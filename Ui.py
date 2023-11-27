4bb62553790cb17e2efe2a68c551139bed58f3ce
Анастасия <dfghjk>
2023-11-27 18:48:34 +1000

    first commit

---
+++ Ui.py
@@ -0,0 +1,32 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1000, 1000)
+        MainWindow.setContextMenuPolicy(QtCore.Qt.DefaultContextMenu)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(150, 140, 391, 211))
+        font = QtGui.QFont()
+        font.setPointSize(40)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 680, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Rounds!"))
---
+++ main.py
@@ -0,0 +1,40 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QGridLayout
+from PyQt5.QtGui import QPainter, QColor, QPen, QPixmap
+import sys
+import random
+from Ui import Ui_MainWindow
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setFixedSize(self.size().width(), self.size().height())
+        self.pushButton.clicked.connect(self.aply_rounds)
+        self.label = QLabel()
+        canvas = QPixmap(700, 700)
+        self.label.setPixmap(canvas)
+
+        layout = QGridLayout(self.centralwidget)
+        layout.addWidget(self.pushButton, 0, 0)
+        layout.addWidget(self.label, 1, 0)
+
+    def aply_rounds(self):
+        x, y = random.randint(10, 500), random.randint(2, 700)
+        now = random.randint(0, 400)
+        w, h = [now, now]
+        painter = QPainter(self.label.pixmap())
+        pen = QPen()
+        pen.setWidth(5)
+        pen.setColor(QColor(*[255, 255, 0]))
+        painter.setPen(pen)
+        painter.drawEllipse(x, y, w, h)
+        painter.end()
+        self.update()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MainWindow()
+    ex.show()
+    app.exec_()
